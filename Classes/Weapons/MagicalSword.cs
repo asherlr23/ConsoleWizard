1d84f22cbcb4add99e4f7219b0ab7a1bb8c74089
Asherlr2 <73479844+asherlr23@users.noreply.github.com>
2022-07-08 13:12:39 -0400

    Move Weapons folder to Classes
    Since the weapons in the weapons folder are just classes, I moved them to the classes folder.

---
+++ Classes/Weapons/MagicalSword.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Net;
+
+    public class MagicalSword 
+    {
+      public MagicalSword(int Damage) {
+            
+      }
+
+
+
+    }
--- Weapons/MagicalSword.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.IO;
-
-    protected class MagicalSword 
-    {
-
-    }
